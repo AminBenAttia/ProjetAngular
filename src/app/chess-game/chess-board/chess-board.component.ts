e2c05479e20d1519bc15b3ec1244c7190db2f563
AminBenAttia <aminbenattia47@gmail>
2024-12-09 18:38:55 +0100

    Chessboard is now a canvas

--- src/app/chess-game/chess-board/chess-board.component.ts
+++ src/app/chess-game/chess-board/chess-board.component.ts
@@ -1,15 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ChessPieceComponent } from "../chess-piece/chess-piece.component";
 
 @Component({
   selector: 'app-chess-board',
   standalone:true,
   templateUrl: './chess-board.component.html',
   styleUrls: ['./chess-board.component.css'],
-  imports:[CommonModule]
+  imports: [CommonModule, ChessPieceComponent]
 })
 export class ChessBoardComponent {
   rows = Array(8).fill(0);
   cols = Array(8).fill(0);
   letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+
+  ngOnInit(){
+    let canvas= document.getElementById("canvas")! as HTMLCanvasElement;
+    let ctx = canvas.getContext("2d")!;
+    for ( let i= 0; i < 8; i += 2) {
+      for (let j = 0; j < 8; j++) {
+        if (j % 2 == 0) {
+          ctx.fillStyle = "#373855";
+          ctx.fillRect(i * 75, j * 75, 75, 75);
+          ctx.fillStyle = "#FFFFFF";
+          ctx.fillRect((i + 1) * 75, j * 75, 75, 75);
+        } else {
+          ctx.fillStyle = "#FFFFFF";
+          ctx.fillRect(i * 75, j * 75, 75, 75);
+          ctx.fillStyle = "#373855";
+          ctx.fillRect((i + 1) * 75, j * 75, 75, 75);
+        }
+    }
+  }
+}
 }
---
+++ src/app/chess-game/chess-piece/chess-piece.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-chess-piece',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './chess-piece.component.html',
+  styleUrls: ['./chess-piece.component.css']
+})
+export class ChessPieceComponent {
+
+}
